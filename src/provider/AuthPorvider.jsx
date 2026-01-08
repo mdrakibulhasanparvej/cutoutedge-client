484b581f1f508d70e27f90c327991b8e9906eb05
mdrakibulhasanparvej <mdrakibulhasanparvej@gmail.com>
2026-01-09 01:20:14 +0600

    folders file added

---
+++ src/provider/AuthPorvider.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AuthPorvider = () => {
+  return (
+    <div>
+      <h2>Auth AuthPorvider</h2>
+    </div>
+  );
+};
+
+export default AuthPorvider;
---
+++ src/routes/Router.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Router = () => {
+  return (
+    <div>
+      <h2>Routers</h2>
+    </div>
+  );
+};
+
+export default Router;
