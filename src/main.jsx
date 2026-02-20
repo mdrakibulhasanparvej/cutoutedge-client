f904d9b16d3cca1fe1cb1f3523bcaf7d60ee1a8b
Md Rakibul Hasan Parvej <mdrakibulhasanparvej@gmail.com>
2026-02-21 00:11:39 +0600

    authprovider wraped

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import "./index.css";
 import { RouterProvider } from "react-router";
 import router from "./routes/Router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AuthProvider from "./provider/AuthProvider";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
-  </StrictMode>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </AuthProvider>
+  </StrictMode>,
 );
